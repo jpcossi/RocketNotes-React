42f8e3b84ba6a38804e44bd08cec8dea39747691
jpcossi <jppcossi@gmail.com>
2022-10-13 16:06:21 -0300

    Início do css e do theme

---
+++ rocketnotes/src/Pages/App/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles.js'
+
+export function App(){  
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+      <span>João Paulo Cossi</span>
+    </Container>
+  )
+}
