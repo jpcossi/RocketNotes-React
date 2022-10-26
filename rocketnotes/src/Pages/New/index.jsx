135c553ba61b9294a2d9d55cecf76c31c5483272
jpcossi <jppcossi@gmail.com>
2022-10-26 18:54:23 -0300

    finalização page new

--- rocketnotes/src/Pages/New/index.jsx
+++ rocketnotes/src/Pages/New/index.jsx
@@ -1,7 +1,10 @@
-import { Header } from '../../components/Header'
-import { Input } from '../../components/Input'
-import { TextArea } from '../../components/Textarea'
 import { Container, Form } from './styles'
+import { TextArea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
 
 export function New(){
     return(
@@ -15,6 +18,18 @@ export function New(){
                     </header>
                     <Input placeholder="Título"></Input>
                     <TextArea placeholder="Observações"></TextArea>
+                    <Section title="Links úteis">
+                        <NoteItem value="htt"></NoteItem>
+                        <NoteItem isNew placeholder="Novo link"></NoteItem>
+                    </Section>
+                    <Section title="Marcadores">
+                        <div className='tags'>    
+                            <NoteItem value="react"></NoteItem>
+                            <NoteItem isNew placeholder="Nova Tag"></NoteItem>
+                        </div>
+                    </Section>
+                    <Button title="Salvar"></Button>
+
                 </Form>
             </main>
         </Container>
